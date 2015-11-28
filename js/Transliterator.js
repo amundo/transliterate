74ca9c2126cd017132ad7fd675d4b0bc4255d048
Patrick Hall <pjh@umail.ucsb.edu>
2015-11-28 15:46:06 -0800

    page

---
+++ js/Transliterator.js
@@ -0,0 +1,59 @@
+"use strict";
+
+var _createClass = (function () { function defineProperties(target, props) { for (var key in props) { var prop = props[key]; prop.configurable = true; if (prop.value) prop.writable = true; } Object.defineProperties(target, props); } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } };
+
+var Transliterator = (function () {
+  function Transliterator(alphabet) {
+    _classCallCheck(this, Transliterator);
+
+    this.alphabet = alphabet;
+  }
+
+  _createClass(Transliterator, {
+    escape: {
+
+      // if a string to be transliterated contains any characters which have special
+      // meaning in regular expressions, escape them
+
+      value: function escape(raw) {
+        return raw.replace(/([.*+?^${}()|\[\]\/\\])/g, "\\$1");
+      }
+    },
+    transliterate: {
+      value: function transliterate(from, to, text) {
+        var _this = this;
+
+        if (!this.alphabet) {
+          return text;
+        }var rules = this.alphabet.map(function (grapheme) {
+          return [grapheme[from], grapheme[to]];
+        });
+
+        //console.log(rules.map(r => `${r[0]} ${r[1]}`).join('\n'));
+
+        rules.sort(function (a, b) {
+
+          var feeding = function (a, b) {
+            return a[1] == b[0];
+          };
+
+          return feeding || a[0].length < b[0].length ? -1 : 1;
+        });
+
+        rules.forEach(function (rule) {
+          var before = _this.escape(rule[0]),
+              after = _this.escape(rule[1]),
+              re = new RegExp(before, "g");
+
+          text = text.replace(re, rule[1]);
+        });
+
+        return text;
+      }
+    }
+  });
+
+  return Transliterator;
+})();
---
+++ js/test/spec/TransliteratorSpec.js
@@ -0,0 +1,83 @@
+describe('Transliterator', function(){
+  beforeEach(function(done){
+    var init = rules => {
+      this.t = new Transliterator(rules);
+    }
+
+    var url = 'http://glyph.local/~pat/Transliterate/transliterate/js/chatino-alphabet.json';
+    
+    fetch(url)
+      .then(r => r.json())
+      .then(init)
+      .then(done)
+      .catch(e => console.log(e))
+
+
+  })
+
+  describe('initializes', function(){
+
+    it('inits', function(){
+      expect(this.t).not.toBeNull();
+    })
+
+    it('has a transliterate method', function(){
+      expect(this.t.transliterate).not.toBeNull();
+    })
+  })
+
+  describe('Chatino rules', function(){
+    beforeEach(function(){
+      this.sample = `
+le!e_
+kee lira_
+kya7a_ ke!ku_
+ya ntzuti
+lya_ta! yaa_7
+serli_
+nyate_& nu n-ch-u7u to laa
+tzu!7na_
+nku_-tzu!7
+xi7na_ kii7
+keto&7 ki_kwa!&
+%-ata7
+ti!kwi_
+jlyani_
+ki_kwa!& nu nti-ka jne_
+ni7i n-tz-u7u n-tz-ukwa_7
+nu nt-i-7ni=nta_wi!7 sapatu_
+kwi!yu_7
+ki_kwa!& nt-u_-sa_7a!& kwe_ya!7`
+    })
+
+    it('IPA[a] => PDLMA[a]', function(){
+      expect(this.t.transliterate('ipa', 'PDLMA', 'a' )).toBe('a');
+    })
+
+    it('IPA[j] => PDLMA[y]', function(){
+      expect(this.t.transliterate('ipa', 'PDLMA', 'j' )).toBe('y');
+    })
+
+    it('PDLMA[y] =>  practical[y]', function(){
+      expect(this.t.transliterate('PDLMA', 'practical', 'y' )).toBe('y');
+    })
+
+    it('PDLMA[sh] =>  practical[x]', function(){
+      expect(this.t.transliterate('PDLMA', 'practical', 'sh' )).toBe('x');
+    })
+
+    it('PDLMA[sh] =>  practical[x]', function(){
+      expect(this.t.transliterate('PDLMA', 'practical', 'sh' )).toBe('x');
+    })
+
+    it('reversible PDLMA, practical', function(){
+      var practical = this.t.transliterate('PDLMA', 'practical', this.sample );
+      var PDLMA = this.t.transliterate('practical', 'PDLMA', practical );
+      var practical2 = this.t.transliterate('PDLMA', 'practical', PDLMA );
+      expect(practical).toEqual(practical2);
+    })
+   
+  })
+
+
+})
